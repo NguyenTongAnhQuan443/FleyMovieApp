8e97cfa06746f9a75030feba9e261c79c8841a6e
NguyenTongAnhQuan443 <ntanhquan.sly@gmail.com>
2024-07-09 00:08:40 +0700

    Update function video rewind, lock screen, code optimization

--- lib/views/play_video_screen/play_video_screen.dart
+++ lib/views/play_video_screen/play_video_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 import 'package:flutter/services.dart';
@@ -7,7 +6,7 @@ import 'package:flutter/services.dart';
 class PlayVideoScreen extends StatefulWidget {
   final String movieUrl;
 
-  PlayVideoScreen(this.movieUrl, {Key? key}) : super(key: key);
+  const PlayVideoScreen(this.movieUrl, {super.key});
 
   @override
   State<StatefulWidget> createState() {
@@ -23,6 +22,7 @@ class _PlayVideoScreenState extends State<PlayVideoScreen> {
   bool _showControls = true;
   Timer? _hideControlsTimer;
   bool _isPause = false;
+  bool _lockScreen = false;
 
   _PlayVideoScreenState(this.movieUrl);
 
@@ -34,7 +34,10 @@ class _PlayVideoScreenState extends State<PlayVideoScreen> {
       DeviceOrientation.landscapeRight,
       DeviceOrientation.landscapeLeft,
     ]);
-    _controller = VideoPlayerController.network(movieUrl)
+    // Hide the status bar and navigation bar
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+
+    _controller = VideoPlayerController.networkUrl(Uri.parse(movieUrl))
       ..initialize().then((_) {
         setState(() {
           _totalDuration = _controller.value.duration.inSeconds.toDouble();
@@ -50,35 +53,79 @@ class _PlayVideoScreenState extends State<PlayVideoScreen> {
   @override
   void dispose() {
     _controller.dispose();
-    // Unlock the screen orientation
+    // Unlock the screen orientation and show the status bar and navigation bar
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
       DeviceOrientation.landscapeRight,
       DeviceOrientation.landscapeLeft
     ]);
-    _hideControlsTimer?.cancel(); // Cancel the timer when disposing the screen
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    _hideControlsTimer?.cancel();
     super.dispose();
   }
 
   void _toggleControlsVisibility() {
-    setState(() {
-      _showControls = !_showControls;
-    });
-    if (_showControls) {
-      _startTimerToHideControls();
+    if (!_lockScreen) {
+      setState(() {
+        _showControls = !_showControls;
+        if (_showControls) {
+          SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+        } else {
+          SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+        }
+      });
+      if (_showControls && !_isPause) {
+        _startTimerToHideControls();
+      } else {
+        _hideControlsTimer?.cancel();
+      }
     } else {
-      _hideControlsTimer?.cancel();
+      // When screen is locked, only show the lock icon
+      setState(() {
+        _showControls = false;
+      });
     }
   }
 
   void _startTimerToHideControls() {
     _hideControlsTimer?.cancel(); // Cancel any previous timer
-    _hideControlsTimer = Timer(const Duration(seconds: 3), () {
+    _hideControlsTimer = Timer(const Duration(seconds: 4), () {
       setState(() {
-        _showControls = false;
+        if (!_isPause && !_lockScreen) {
+          _showControls = false;
+          SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+        }
       });
     });
+  }
+
+  void _togglePlayPause() {
+    setState(() {
+      if (_controller.value.isPlaying) {
+        _controller.pause();
+        _isPause = true;
+        _hideControlsTimer?.cancel(); // Cancel timer when paused
+      } else {
+        _controller.play();
+        _isPause = false;
+        _startTimerToHideControls(); // Restart timer when playing
+      }
+    });
+  }
+
+  void _replayVideo() {
+    final newPosition =
+        _controller.value.position - const Duration(seconds: 10);
+    _controller
+        .seekTo(newPosition > Duration.zero ? newPosition : Duration.zero);
+  }
+
+  void _forwardVideo() {
+    final newPosition =
+        _controller.value.position + const Duration(seconds: 10);
+    final duration = _controller.value.duration;
+    _controller.seekTo(newPosition < duration ? newPosition : duration);
   }
 
   Orientation get orientation => MediaQuery.of(context).orientation;
@@ -96,88 +143,25 @@ class _PlayVideoScreenState extends State<PlayVideoScreen> {
             Center(
               child: _controller.value.isInitialized
                   ? Stack(
-                alignment: Alignment.bottomCenter,
-                children: [
-                  AspectRatio(
-                    aspectRatio: orientation == Orientation.portrait
-                        ? 16 / 9
-                        : screenWidth / screenHeight,
-                    child: VideoPlayer(_controller),
-                  ),
-                  if (_showControls)
-                    Positioned(
-                      bottom: 42,
-                      left: 5,
-                      right: 5,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            formatDuration(_currentSliderValue.toInt()),
-                            style: const TextStyle(color: Colors.grey),
-                          ),
-                          Text(
-                            formatDuration(_totalDuration.toInt()),
-                            style: const TextStyle(color: Colors.grey),
-                          ),
-                        ],
-                      ),
-                    ),
-                  if (_showControls)
-                    Container(
-                      margin: const EdgeInsets.only(
-                          left: 70, right: 70, bottom: 50),
-                      width: double.infinity,
-                      height: 8,
-                      child: VideoProgressIndicator(
-                        _controller,
-                        allowScrubbing: true,
-                        colors: VideoProgressColors(
-                          playedColor: Colors.red,
-                          backgroundColor: Colors.black.withOpacity(0.5),
+                      alignment: Alignment.bottomCenter,
+                      children: [
+                        AspectRatio(
+                          aspectRatio: orientation == Orientation.portrait
+                              ? 16 / 9
+                              : screenWidth / screenHeight,
+                          child: VideoPlayer(_controller),
                         ),
-                      ),
-                    ),
-                ],
-              )
+                        if (_showControls && !_lockScreen) buildDuration(),
+                        if (_showControls && !_lockScreen) buildProgressVideo(),
+                      ],
+                    )
                   : const CircularProgressIndicator(
-                color: Colors.white,
-              ),
+                      color: Colors.white,
+                    ),
             ),
-            if (_showControls)
-              Positioned(
-                top: 20,
-                left: 20,
-                child: IconButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  icon: const Icon(
-                    Icons.arrow_back,
-                    color: Colors.grey,
-                  ),
-                ),
-              ),
-            Positioned(
-              top: screenHeight / 2 - 30,
-              left: screenWidth / 2 - 30,
-              child: IconButton(
-                onPressed: () {
-                  setState(() {
-                    _controller.value.isPlaying
-                        ? (_controller.pause(), _isPause = true)
-                        : (_controller.play(), _isPause = false);
-                  });
-                  _toggleControlsVisibility(); // Show controls when play/pause button is pressed
-                },
-                icon: Icon(
-                  _controller.value.isPlaying
-                      ? Icons.pause
-                      : Icons.play_arrow,
-                  size: 40,
-                ),
-              ),
-            ),
+            if (_showControls && !_lockScreen) buildIconBack(),
+            if (_showControls && !_lockScreen) buildReplayPauseforwardVideo(),
+            buildLockScreen(),
           ],
         ),
       ),
@@ -190,5 +174,144 @@ class _PlayVideoScreenState extends State<PlayVideoScreen> {
     String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
     String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
     return "${duration.inHours > 0 ? '${twoDigits(duration.inHours)}:' : ''}$twoDigitMinutes:$twoDigitSeconds";
+  }
+
+  // Build Icon Back
+  Widget buildIconBack() {
+    return Positioned(
+      top: 20,
+      left: 20,
+      child: IconButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        icon: const Icon(
+          Icons.arrow_back,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  // Build Replay-Pause-forward-Video
+  Widget buildReplayPauseforwardVideo() {
+    return Positioned.fill(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    _replayVideo();
+                  },
+                  icon: const Icon(
+                    Icons.replay_10,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                ),
+                const SizedBox(
+                  width: 180,
+                ),
+                IconButton(
+                  onPressed: _togglePlayPause,
+                  icon: Icon(
+                    _controller.value.isPlaying
+                        ? Icons.pause
+                        : Icons.play_arrow,
+                    size: 40,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(
+                  width: 180,
+                ),
+                IconButton(
+                  onPressed: () {
+                    _forwardVideo();
+                  },
+                  icon: const Icon(
+                    Icons.forward_10,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Widget Duration
+  Widget buildDuration() {
+    return Positioned(
+      bottom: 42,
+      left: 5,
+      right: 5,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            formatDuration(_currentSliderValue.toInt()),
+            style: const TextStyle(color: Colors.white),
+          ),
+          Text(
+            formatDuration(_totalDuration.toInt()),
+            style: const TextStyle(color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+
+// Widget build Progress video
+  Widget buildProgressVideo() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Container(
+          margin: const EdgeInsets.only(left: 70, right: 70, bottom: 50),
+          width: double.infinity,
+          height: 8,
+          child: VideoProgressIndicator(
+            _controller,
+            allowScrubbing: true,
+            colors: VideoProgressColors(
+              playedColor: Colors.red,
+              backgroundColor: Colors.black.withOpacity(0.5),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildLockScreen() {
+    return Positioned(
+      bottom: 10,
+      right: 10,
+      child: IconButton(
+        onPressed: () {
+          setState(() {
+            _lockScreen = !_lockScreen;
+            if (_lockScreen) {
+              SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+            } else {
+              SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+            }
+          });
+        },
+        icon: Icon(
+          (_lockScreen) ? Icons.lock_outline_rounded : Icons.lock_open_outlined,
+          color: Colors.white,
+          size: 20,
+        ),
+      ),
+    );
   }
 }
